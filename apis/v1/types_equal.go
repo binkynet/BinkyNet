0d1b2217583eb1e52ccba6c0ec9d598946ff4f0d
Ewout Prangsma <ewout@prangsma.net>
2022-02-19 15:29:23 +0100

    Added Equal helpers for LocalWorkerConfig

--- apis/v1/types_equal.go
+++ apis/v1/types_equal.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Ewout Prangsma
+// Copyright 2020-2022 Ewout Prangsma
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -92,4 +92,109 @@ func (s *Clock) Equal(o *Clock) bool {
 	return s.GetPeriod() == o.GetPeriod() &&
 		s.GetHours() == o.GetHours() &&
 		s.GetMinutes() == o.GetMinutes()
+}
+
+// Equal returns true when both objects have the same values.
+func (s *Device) Equal(o *Device) bool {
+	return s.GetId() == o.GetId() &&
+		s.GetType() == o.GetType() &&
+		s.GetAddress() == o.GetAddress()
+}
+
+// Equal returns true when both objects have the same values.
+func (s *Object) Equal(o *Object) bool {
+	return s.GetId() == o.GetId() &&
+		s.GetType() == o.GetType() &&
+		s.equalConnections(o)
+}
+
+// equalConnections returns true when Connections in both objects have the same values.
+func (s *Object) equalConnections(o *Object) bool {
+	if len(s.GetConnections()) != len(o.GetConnections()) {
+		return false
+	}
+	ocs := o.GetConnections()
+	for i, sc := range s.GetConnections() {
+		if !sc.Equal(ocs[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+// Equal returns true when both objects have the same values.
+func (s *Connection) Equal(o *Connection) bool {
+	return s.GetKey() == o.GetKey() &&
+		s.equalPins(o) &&
+		s.equalConfiguration(o)
+}
+
+// equalPins returns true when Pins in both objects have the same values.
+func (s *Connection) equalPins(o *Connection) bool {
+	if len(s.GetPins()) != len(o.GetPins()) {
+		return false
+	}
+	ops := o.GetPins()
+	for i, sc := range s.GetPins() {
+		if !sc.Equal(ops[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+// equalConfiguration returns true when Configuration in both objects have the same values.
+func (s *Connection) equalConfiguration(o *Connection) bool {
+	if len(s.GetConfiguration()) != len(o.GetConfiguration()) {
+		return false
+	}
+	ocs := o.GetConfiguration()
+	for k, sv := range s.GetConfiguration() {
+		ov := ocs[k]
+		if sv != ov {
+			return false
+		}
+	}
+	return true
+}
+
+// Equal returns true when both objects have the same values.
+func (s *DevicePin) Equal(o *DevicePin) bool {
+	return s.GetDeviceId() == o.GetDeviceId() &&
+		s.GetDeviceIndex() == o.GetDeviceIndex()
+}
+
+// Equal returns true when both objects have the same values.
+func (s *LocalWorkerConfig) Equal(o *LocalWorkerConfig) bool {
+	return s.GetAlias() == o.GetAlias() &&
+		s.equalDevices(o) &&
+		s.equalObjects(o)
+}
+
+// equalDevices returns true when both objects have the same devices.
+func (s *LocalWorkerConfig) equalDevices(o *LocalWorkerConfig) bool {
+	if len(s.GetDevices()) != len(o.GetDevices()) {
+		return false
+	}
+	ods := o.GetDevices()
+	for i, sc := range s.GetDevices() {
+		if !sc.Equal(ods[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+// equalObjects returns true when both objects have the same devices.
+func (s *LocalWorkerConfig) equalObjects(o *LocalWorkerConfig) bool {
+	if len(s.GetObjects()) != len(o.GetObjects()) {
+		return false
+	}
+	oos := o.GetObjects()
+	for i, sc := range s.GetObjects() {
+		if !sc.Equal(oos[i]) {
+			return false
+		}
+	}
+	return true
 }
