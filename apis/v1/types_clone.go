68d278ee4b55152ff759df5628ca68fd3cafd94f
Ewout Prangsma <ewout@prangsma.net>
2022-07-05 15:51:21 +0200

    Added clone helpers

--- apis/v1/types_clone.go
+++ apis/v1/types_clone.go
@@ -18,6 +18,91 @@
 package v1
 
 // Clone the entire object
+func (s *Device) Clone() *Device {
+	if s == nil {
+		return nil
+	}
+	clone := *s
+	return &clone
+}
+
+// Clone the entire object
+func (s *Object) Clone() *Object {
+	if s == nil {
+		return nil
+	}
+	clone := *s
+	clone.Connections = make([]*Connection, 0, len(s.GetConnections()))
+	for _, x := range s.GetConnections() {
+		clone.Connections = append(clone.Connections, x.Clone())
+	}
+	return &clone
+}
+
+// Clone the entire object
+func (s *Connection) Clone() *Connection {
+	if s == nil {
+		return nil
+	}
+	clone := *s
+	clone.Pins = make([]*DevicePin, 0, len(s.GetPins()))
+	for _, x := range s.GetPins() {
+		clone.Pins = append(clone.Pins, x.Clone())
+	}
+	clone.Configuration = make(map[ConfigKey]string)
+	for k, v := range s.Configuration {
+		clone.Configuration[k] = v
+	}
+	return &clone
+}
+
+// Clone the entire object
+func (s *DevicePin) Clone() *DevicePin {
+	if s == nil {
+		return nil
+	}
+	clone := *s
+	return &clone
+}
+
+// Clone the entire object
+func (s *LocalWorkerConfig) Clone() *LocalWorkerConfig {
+	if s == nil {
+		return nil
+	}
+	clone := *s
+	clone.Devices = make([]*Device, 0, len(s.GetDevices()))
+	for _, x := range s.GetDevices() {
+		clone.Devices = append(clone.Devices, x.Clone())
+	}
+	clone.Objects = make([]*Object, 0, len(s.GetObjects()))
+	for _, x := range s.GetObjects() {
+		clone.Objects = append(clone.Objects, x.Clone())
+	}
+	return &clone
+}
+
+// Clone the entire object
+func (s *LocalWorkerInfo) Clone() *LocalWorkerInfo {
+	if s == nil {
+		return nil
+	}
+	clone := *s
+	return &clone
+}
+
+// Clone the entire object
+func (s *LocalWorker) Clone() *LocalWorker {
+	if s == nil {
+		return nil
+	}
+	clone := *s
+	clone.Request = s.GetRequest().Clone()
+	clone.Actual = s.GetActual().Clone()
+	return &clone
+}
+
+// Clone the entire object
 func (s *PowerState) Clone() *PowerState {
 	if s == nil {
 		return nil
